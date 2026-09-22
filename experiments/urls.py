8f36859f4dd8e1a3bd0d66d73d55d978520b1786
tltkalinin <tltkalinin@gmail.com>
2026-09-22 12:41:51 +0300

    Add user login and logout

--- experiments/urls.py
+++ experiments/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 app_name = "experiments"
 urlpatterns = [
+    path("login/", LoginView.as_view(template_name="experiments/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("", views.index, name="index"),
     path("export/", views.export_excel, name="export_excel"),
 ]
--- sinterlab/settings.py
+++ sinterlab/settings.py
@@ -126,3 +126,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'experiments:login'
+LOGIN_REDIRECT_URL = 'experiments:index'
+LOGOUT_REDIRECT_URL = 'experiments:index'
